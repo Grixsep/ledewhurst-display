021decd09b93c0de675a2e7a7539387366805334
Paul Dewhurst <35234570+Grixsep@users.noreply.github.com>
2025-04-29 20:19:05 -0500

    fixes for gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // ← this enables static HTML export
+  images: {
+    unoptimized: true, // required for export
+  },
   sassOptions: {
     compiler: "modern",
     silenceDeprecations: ["legacy-js-api"],
